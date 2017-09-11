01f3c4adebac0ca88d5d904018b10fd834c34e18
MattRobertson <94matt@live.com.au>
2017-09-11 11:50:29 +1000

    Our House

---
+++ demos/trades/large/wp-login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Trades Theme v1 &lsaquo; Log In</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='/demos/trades/large/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.1' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="/demos/trades/large/wp-content/uploads/2017/09/cropped-logo-32x32.png" sizes="32x32" />
+<link rel="icon" href="/demos/trades/large/wp-content/uploads/2017/09/cropped-logo-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/demos/trades/large/wp-content/uploads/2017/09/cropped-logo-180x180.png" />
+<meta name="msapplication-TileImage" content="/wp-content/uploads/2017/09/cropped-logo-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Trades Theme v1</a></h1>
+	
+<form name="loginform" id="loginform" action="/demos/trades/large/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/demos/trades/large/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/demos/trades/large/">&larr; Back to Trades Theme v1</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
